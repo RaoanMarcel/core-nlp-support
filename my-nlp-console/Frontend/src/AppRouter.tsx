13dc9cd2013112d5b7c38af1ddf4e2f8f057a52c
Raoan Marcel <raoanmarceljukowski@gmail.com>
2026-04-18 17:41:50 -0300

    Refatoração de pastas raizes

--- my-nlp-console/Frontend/src/AppRouter.tsx
+++ my-nlp-console/Frontend/src/AppRouter.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
-import AppLayout from './features/ProspectList/components/SidebarLayout';
+import AppLayout from './components/SidebarLayout';
 import Dashboard from './components/Dashboard';
 import TicketQueue from './components/TicketQueue';
 import KnowledgeBase from './components/KnowledgeBase';
